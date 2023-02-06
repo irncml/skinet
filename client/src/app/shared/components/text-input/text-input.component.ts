1e05f92338874d4c6143e1f3282f6279891893bf
Cemil <cdemir.cd10@gmail.com>
2023-02-06 18:44:50 +0300

    End of section 17

---
+++ client/src/app/shared/components/text-input/text-input.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, Self } from '@angular/core';
+import { NgControl, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.css']
+})
+export class TextInputComponent {
+  @Input() type = 'text';
+  @Input() label = '';
+
+  constructor(@Self() public controlDir: NgControl) {
+    this.controlDir.valueAccessor = this;
+  }
+
+  writeValue(obj: any): void {
+  }
+  registerOnChange(fn: any): void {
+  }
+  registerOnTouched(fn: any): void {
+  }
+
+  get control(): FormControl {
+    return this.controlDir.control as FormControl
+  }
+}
